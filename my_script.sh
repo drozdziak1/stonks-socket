eb0d7a671b633b76f517b930e40a831f6e908142
Stan Drozd <drozdziak1@gmail.com>
2021-10-31 04:06:22 +0100

    Add sender.py and my_script.sh

---
+++ my_script.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -x
+ls -la /mnt
+mount
+whoami
+cat /mnt/flag
---
+++ sender.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+with open("my_script.sh") as f:
+    s = f.read()
+    print(len(s))
+    print(s)
